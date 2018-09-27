1e01ec4414c5ce1af6bc5942a7246505c07a5d92
Ryan-Corcoran <ryan_corcoran@live.co.uk>
2018-09-27 14:19:32 +0100

    new scala file created for tests rather than worksheet and unit tests created

--- DDCT_Recruitment_Exercise/src/test/scala/CafeXTests.scala
+++ DDCT_Recruitment_Exercise/src/test/scala/CafeXTests.scala
@@ -1,18 +1,5 @@
 import org.scalatest.FunSuite
-import main.scala.*
+
 
 class CafeXTests extends FunSuite {
-
-    //Testing A equals 3.5
-    test(A) {
-      Assert(A == 3.5)
-    }
-
-    test(B) {
-      Assert(B == 1.00)
-    }
-
-    test(C) {
-      Assert(C == 0.5)
-    }
 }
