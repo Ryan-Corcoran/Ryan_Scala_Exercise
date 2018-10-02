6ecf13db356fe34c1d83b116100055be362676ca
Ryan-Corcoran <ryan_corcoran@live.co.uk>
2018-10-02 15:02:52 +0100

    vars now removed

--- DDCT_Recruitment_Exercise/src/main/scala/MenuItem.scala
+++ DDCT_Recruitment_Exercise/src/main/scala/MenuItem.scala
@@ -2,5 +2,5 @@
 Class designed to hold values for each menu item. Called in CafeX to store the values of all menu items
 @Author - Ryan Corcoran
  */
-case class MenuItem(name: String, temp: String, cat: String, price: Double) {
+case class MenuItem(name: String, temp: String, cat: String, price: Double, SCPercent: Double) {
 }
